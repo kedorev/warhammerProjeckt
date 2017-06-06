14d5e0f61081d4dab3ec3538e180a3fdd96e99e5
kedorev <kedorev@gmail.com>
2017-06-06 17:17:54 +0200

    Add bootstrap Add assetic Begining of templatating

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,19 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
+
+
+            /**
+             * Create Bundel
+             */
             new MainAppBundle\MainAppBundle(),
+
+            /**
+             * Added external bundle
+             */
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/MainAppBundle/Controller/DefaultController.php
+++ src/MainAppBundle/Controller/DefaultController.php
@@ -3,16 +3,28 @@
 namespace MainAppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction( Request $request )
     {
+        $session = $request->getSession();
+        if(!isset($session))
+        {
+            $session = new Session();
+            $session->set("test","debug");
+        }
+
         return $this->render('MainAppBundle:Default:index.html.twig');
     }
 
-    public function modelAction()
+    public function modelAction(Request $request)
     {
+        $session = $request->getSession();
+
+
         return $this->render('MainAppBundle:Default:Model.html.twig');
     }
 
