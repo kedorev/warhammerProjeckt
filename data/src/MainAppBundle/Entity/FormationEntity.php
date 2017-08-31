d6419c17b59bb9a13b029a7fa45e65fbf2aaaaeb
kedorev <kedorev@gmail.com>
2017-08-31 12:13:01 +0200

    Update liste and adding relation between formatinsEntity

---
+++ data/src/MainAppBundle/Entity/FormationEntity.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace MainAppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * FormationEntity
+ *
+ * @ORM\Table(name="formation_entity")
+ * @ORM\Entity(repositoryClass="MainAppBundle\Repository\FormationEntityRepository")
+ */
+class FormationEntity
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * @var Liste
+     * @ORM\ManyToOne(targetEntity="MainAppBundle\Entity\Liste")
+     */
+    private $list;
+
+
+    /**
+     * @var array
+     *
+     * @ORM\OneToMany(targetEntity="MainAppBundle\Entity\SquadsEntity", mappedBy="list")
+     */
+    private $SquadsEntity;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->SquadsEntity = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Set list
+     *
+     * @param \MainAppBundle\Entity\Liste $list
+     *
+     * @return FormationEntity
+     */
+    public function setList(\MainAppBundle\Entity\Liste $list = null)
+    {
+        $this->list = $list;
+
+        return $this;
+    }
+
+    /**
+     * Get list
+     *
+     * @return \MainAppBundle\Entity\Liste
+     */
+    public function getList()
+    {
+        return $this->list;
+    }
+
+    /**
+     * Add squadsEntity
+     *
+     * @param \MainAppBundle\Entity\SquadsEntity $squadsEntity
+     *
+     * @return FormationEntity
+     */
+    public function addSquadsEntity(\MainAppBundle\Entity\SquadsEntity $squadsEntity)
+    {
+        $this->SquadsEntity[] = $squadsEntity;
+
+        return $this;
+    }
+
+    /**
+     * Remove squadsEntity
+     *
+     * @param \MainAppBundle\Entity\SquadsEntity $squadsEntity
+     */
+    public function removeSquadsEntity(\MainAppBundle\Entity\SquadsEntity $squadsEntity)
+    {
+        $this->SquadsEntity->removeElement($squadsEntity);
+    }
+
+    /**
+     * Get squadsEntity
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getSquadsEntity()
+    {
+        return $this->SquadsEntity;
+    }
+}
